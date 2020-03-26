7fbe99fb055943cc17a1de1e815f6591987bd296
JasonLuis <jason20luis@gmail.com>
2020-03-26 01:55:37 -0300

    Aplicação do Be The Hero

---
+++ backend/src/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes')
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+/**
+ * Rotas / Recursos
+ * 
+ * /
+ 
+ /** 
+ * Métodos HTTP:
+ *  
+ * GET: buscar uma informação do back-end
+ * POST: Alterar uma informação no back-end
+ * PUT: ALterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+
+/**
+ * Tipos de parametros
+ * 
+ * Query Parms: Parâmetros enviados na rota após o '?' (Filtros, paginação)
+ * Route Parms: Parâmetros utilizados para identificar recursos
+ * Request Body: COrpo da requisição, utilizado para criar ou alterar recursos
+ */
+
+/**
+ * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
+ * NoSQL: MongoDB, CouchDB, etc
+ */
+
+
+ /**
+  * Driver: SELECT * FROM users
+  * Query Builder: table('users').select('*').where()
+  */
+
+
+app.listen(3333);
+
---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './global.css';
+
+import Routes from './routes';
+
+//JSX (JavaScript e XML)
+
+/**
+ * UseState para criar um Estado
+ */
+/**
+ * Umutabilidade, criamos um array que a primeira posição é o valor
+ * e a segunda um função para alterar o valor 
+ */
+function App() {
+
+  return (
+      <Routes/>
+  );
+  
+}
+
+export default App;
